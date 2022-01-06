608e2f87afe306d73c1fc2417cef5cac6682b6a2
Arcanodev <92180339+kaiolabs@users.noreply.github.com>
2022-01-06 20:43:01 -0300

    project-manager

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Container from './components/layout/Container';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import Home from './components/pages/Home';
+import NewProject from './components/pages/NewProject';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <nav>
+          <Link to= "/">Home</Link>
+          <Link to= "/company">Company</Link>
+          <Link to= "/contact">Contact</Link>
+          <Link to= "/newproject">NewProject</Link>      
+      </nav>
+
+      <Container customClass="min_height">
+        <Routes>
+            <Route exact path="/" element={<Home/>}></Route>
+            <Route path="/company" element={<Company/>}></Route>
+            <Route path="/contact" element={<Contact/>}></Route>
+            <Route path="/newproject" element={<NewProject/>}></Route>
+        </Routes>
+      </Container>
+
+        <p>Footer</p>
+
+    </Router>
   );
 }
 
