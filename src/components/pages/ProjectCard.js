517c350036a4c3277f38156a8193b5448806babb
Arcanodev <92180339+kaiolabs@users.noreply.github.com>
2022-01-19 23:17:20 -0300

    Treinos

---
+++ src/components/pages/ProjectCard.js
@@ -0,0 +1,12 @@
+import Styles from  './ProjectCard.module.css';
+import {BsPencil, BsFillTrashFill} from 'react-icons';
+
+function ProjectCard({id, name, budget, category, handleRemove}) {
+    return ( 
+        <div>
+
+        </div>
+     );
+}
+
+export default ProjectCard;
