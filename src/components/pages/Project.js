1b9e82e5ec6d44834a231f2555cba4d116d1b3d7
Arcanodev <92180339+kaiolabs@users.noreply.github.com>
2022-02-04 17:37:37 -0300

    Update

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -6,7 +6,7 @@ import Container from '../layout/Container';
 import ProjectForm from '../project/ProjectForm';
 import Messager from '../layout/Messager';
 import ServiceForm from '../service/ServiceForm';
-import { parse, v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import ServiceCard from '../service/ServiceCard'
 
 function Project() {
